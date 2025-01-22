725da9cb7543493477ed00ae4706509beb533e66
ELBGDELARETOU <adem.naouali@outlook.fr>
2025-01-22 14:27:21 +0100

    cpp09

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,10 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include<vector>
+#include<iostream>
+#include<fstream>
+#include<sstream>
+
+
+#endif
---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,8 @@
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+    if(ac != 3)
+        return (std::cout << "Syntax error: <prog> <csv file>" << std::endl, 0);
+    
+}
