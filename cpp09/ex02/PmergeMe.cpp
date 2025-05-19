9a1f92fec001acffbc1bfb9584c05f5ab06c460f
ELBGDELARETOU <adem.naouali@outlook.fr>
2025-05-19 23:32:41 +0200

    lol

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,11 +1,42 @@
 #include "PmergeMe.hpp"
 
+std::vector<size_t> generateJacobsthalIndices(size_t n) {
+    std::vector<size_t> jacob;
+    jacob.push_back(0);
+    jacob.push_back(1);
+
+    while (true) {
+        size_t next = jacob[jacob.size() - 1] + 2 * jacob[jacob.size() - 2];
+        if (next >= n)
+            break;
+        jacob.push_back(next);
+    }
+
+    std::vector<bool> used(n, false);
+    std::vector<size_t> result;
+
+    for (int i = jacob.size() - 1; i >= 0; --i) {
+        if (jacob[i] < n && !used[jacob[i]]) {
+            result.push_back(jacob[i]);
+            used[jacob[i]] = true;
+        }
+    }
+
+    for (size_t i = 0; i < n; ++i) {
+        if (!used[i])
+            result.push_back(i);
+    }
+
+    return result;
+}
+
 template <typename T>
-typename T::iterator binaryInsert(T& container, int value) {
+typename T::iterator binaryInsert(T& container, int value, size_t& compareCount) {
     typename T::iterator it = container.begin();
     typename T::iterator end = container.end();
 
     while (it < end) {
+        ++compareCount;
         typename T::iterator mid = it + (end - it) / 2;
         if (*mid < value)
             it = mid + 1;
@@ -16,7 +47,7 @@ typename T::iterator binaryInsert(T& container, int value) {
 }
 
 template <typename T>
-void fordJohnsonSort(T& container) {
+void fordJohnsonSort(T& container, size_t& compareCount) {
     if (container.size() <= 1)
         return;
 
@@ -43,37 +74,47 @@ void fordJohnsonSort(T& container) {
     T sortedMax;
     for (size_t i = 0; i < pairs.size(); ++i)
         sortedMax.push_back(pairs[i].first);
-    fordJohnsonSort(sortedMax);
 
-    for (size_t i = 0; i < pairs.size(); ++i) {
-        if (pairs[i].first != pairs[i].second)
-            binaryInsert(sortedMax, pairs[i].second);
+    fordJohnsonSort(sortedMax, compareCount);
+
+    //SANS Jacobsthal
+    // for (size_t i = 0; i < pairs.size(); ++i) {
+    //     if (pairs[i].first != pairs[i].second)
+    //         binaryInsert(sortedMax, pairs[i].second, compareCount);
+    // }
+
+    //AVEC Jacobsthal
+    std::vector<size_t> insertOrder = generateJacobsthalIndices(pairs.size());
+    for (size_t i = 0; i < insertOrder.size(); ++i) {
+        size_t idx = insertOrder[i];
+        if (pairs[idx].first != pairs[idx].second)
+            binaryInsert(sortedMax, pairs[idx].second, compareCount);
     }
+   
 
     container = sortedMax;
 }
 
 template <typename T>
-double measureSort(T& container) {
+double measureSort(T& container, size_t& compareCount) {
     clock_t start = clock();
-    fordJohnsonSort(container);
+    fordJohnsonSort(container, compareCount);
     clock_t end = clock();
     return static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0;
 }
 
 void PmergeMe::sortVector(std::vector<int>& vec) {
-    double duration = measureSort(vec);
+    size_t compareCount = 0;
+    double duration = measureSort(vec, compareCount);
     std::cout << "Time to process a range of " << vec.size()
               << " elements with std::vector : " << duration << " ms" << std::endl;
+    std::cout << "Number of binary comparisons: " << compareCount << std::endl;
 }
 
 void PmergeMe::sortDeque(std::deque<int>& deq) {
-    double duration = measureSort(deq);
+    size_t compareCount = 0;
+    double duration = measureSort(deq, compareCount);
     std::cout << "Time to process a range of " << deq.size()
               << " elements with std::deque  : " << duration << " ms" << std::endl;
+    std::cout << "Number of binary comparisons: " << compareCount << std::endl;
 }
-
-template void fordJohnsonSort(std::vector<int>&);
-template void fordJohnsonSort(std::deque<int>&);
-template double measureSort(std::vector<int>&);
-template double measureSort(std::deque<int>&);
